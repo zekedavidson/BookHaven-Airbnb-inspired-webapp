456de58b3a91b449a139c0ce240c0a937d5dc41c
zekedavidson <bhatiagary28@gmail.com>
2025-09-03 21:49:50 +0530

    filters added for new listing

--- schema.js
+++ schema.js
@@ -7,7 +7,8 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null)
+        image: Joi.string().allow("", null),
+        category: Joi.string().valid("Trending", "Rooms", "Mountains", "Iconic Cities", "Cabins", "Beach", "Camping", "Adventure", "Villas").required()
     }).required()
 });
 
